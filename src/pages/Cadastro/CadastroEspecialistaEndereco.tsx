3ad8cad6d5c788563289e8cc78f92d310def5f32
NeiltonSeguins <seguins.neilton@gmail.com>
2024-02-08 16:51:42 -0300

    Aula 04 - Validando dados de uma API e preenchimento automático

--- src/pages/Cadastro/CadastroEspecialistaEndereco.tsx
+++ src/pages/Cadastro/CadastroEspecialistaEndereco.tsx
@@ -1,12 +1,55 @@
+import { z } from "zod";
 import Botao from "../../components/Botao";
 import CampoDigitacao from "../../components/CampoDigitacao";
 import upload from "/upload.svg";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const esquemaEspecialistaEndereco = z.object({
+  endereco: z.object({
+    cep: z.string().min(9, "Informe um CEP válido"),
+    rua: z.string().min(1, "Informe uma Rua válida"),
+    numero: z.coerce.number().min(1, "Informe um número"),
+    bairro: z.string().min(1, "Informe um Bairro válido"),
+    localidade: z.string().min(1, "Informe uma Localidade válida"),
+  }),
+});
+
+type esquemaEspecialistaEnderecoTipos = z.infer<
+  typeof esquemaEspecialistaEndereco
+>;
 
 const CadastroEspecialistaEndereco = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    criteriaMode: "all",
+    mode: "all",
+    resolver: zodResolver(esquemaEspecialistaEndereco),
+    defaultValues: {
+      endereco: {
+        cep: "",
+        rua: "",
+        numero: 0,
+        bairro: "",
+        localidade: "",
+      },
+    },
+  });
+
+  const aoSubmeter = (dados: esquemaEspecialistaEnderecoTipos) => {
+    console.log(dados);
+  };
+
   return (
     <>
       <h2 className="titulo">Para finalizar, só alguns detalhes!</h2>
-      <form className="formulario__paciente">
+      <form
+        onSubmit={handleSubmit(aoSubmeter)}
+        className="formulario__paciente"
+      >
         <h3 className="titulo__upload">Sua foto</h3>
         <label htmlFor="campo-upload" className="campo__upload">
           <img src={upload} alt="ícone de upload" />
@@ -20,12 +63,16 @@ const CadastroEspecialistaEndereco = () => {
           legenda="Cep"
           tipo="text"
           placeholder="Insira seu CEP"
+          error={errors.endereco?.cep}
+          {...register("endereco.cep")}
         />
         <CampoDigitacao
           id="campo-rua"
           legenda="Rua"
           tipo="text"
           placeholder="Rua Agarikov"
+          error={errors.endereco?.rua}
+          {...register("endereco.rua")}
         />
 
         <div className="formulario__container">
@@ -34,12 +81,16 @@ const CadastroEspecialistaEndereco = () => {
             legenda="Número"
             tipo="text"
             placeholder="Ex: 20"
+            error={errors.endereco?.numero}
+            {...register("endereco.numero")}
           />
           <CampoDigitacao
             id="campo-bairro"
             legenda="Bairro"
             tipo="text"
             placeholder="Vila Mariana"
+            error={errors.endereco?.bairro}
+            {...register("endereco.bairro")}
           />
         </div>
         <CampoDigitacao
@@ -47,6 +98,8 @@ const CadastroEspecialistaEndereco = () => {
           legenda="Localidade"
           tipo="text"
           placeholder="São Paulo, SP"
+          error={errors.endereco?.localidade}
+          {...register("endereco.localidade")}
         />
         <Botao tipo="submit">Cadastrar</Botao>
       </form>
--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,17 +1,12 @@
 import Logotipo from "../../components/Logotipo";
-// import Etapas from "../../components/Etapas";
-import useEtapas from "../../hooks/useEtapas";
-import CadastroPessoal from "./CadastroPessoal";
-import CadastroEndereco from "./CadastroEndereco";
 import "./styles.css";
-import CadastroEspecialistaTecnico from "./CadastroEspecialistaTecnico";
 import CadastroEspecialistaEndereco from "./CadastroEspecialistaEndereco";
 
 export default function Cadastro() {
   return (
     <>
       <Logotipo />
-      <CadastroEspecialistaTecnico />
+      <CadastroEspecialistaEndereco />
     </>
   );
 }
